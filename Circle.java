a1cfd631587191aac718748753f8b55085a3b61b
vaisakhp98 <vaisakhp43@gmail.com>
2022-03-21 19:36:23 +0530

    java Questions 16-03-22

---
+++ Circle.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Circle{
+    public static void main(String[] args){
+        Scanner radius = new Scanner(System.in);
+        System.out.print("Enter the Radius :");
+        double rad = radius.nextDouble();
+
+        double area = Math.PI * rad * rad;
+        double peri = 2 * Math.PI * rad;
+
+        System.out.println("The area of the Circle is " + area);
+        System.out.println("The perimeter of the Circle is " + peri);
+
+    }
+}
---
+++ ProductOfNums.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+class ProductOFNUms{
+    public static void main(String[] args){
+        Scanner First = new Scanner(System.in);
+        System.out.print("1st Number :");
+        int first = First.nextInt();
+        
+        Scanner Second = new Scanner(System.in);
+        System.out.print("2nd Number :");
+        int second = Second.nextInt();
+
+        System.out.println("The Product of " + first + " and " + second + " = " + first*second);
+    }
+}
