9bbae18761738a772dcb585297e63df8ec16ee92
vaisakhp98 <76683034+vaisakhp98@users.noreply.github.com>
2022-04-10 17:04:07 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,25 +1,24 @@
 import java.util.*;
-
 class Palindrome
 {
 	public static void main(String args[])
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number: ");
-		int a = sc.nextInt();
-		if(reverse(a) == a){
-			System.out.print(a+" is palindrome");
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the string: ");
+		String str=sc.nextLine();
+		int size=str.length();
+		boolean ispalindrome=true;
+		for(int i=0;i<size/2;i++)
+		{
+			if(str.charAt(i)!=str.charAt(size-i-1))
+			{
+				ispalindrome=false;
+				break;
+			}
 		}
-		else System.out.print(a+" is not palindrome");
-	}
-
-	public static int reverse(int num)
-	{
-		int reverse=0;
-		while(num!=0){
-			reverse = (reverse * 10) + (num % 10);
-			num/=10;
-		}
-		return reverse;
+		if(ispalindrome)
+			System.out.println("The string "+str+" is a palindrome");
+		else
+			System.out.println("The string "+str+" is not palindrome");
 	}
 }
